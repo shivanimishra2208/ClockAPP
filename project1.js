f3e2d5624626e87f2d7ebfd6d516adc39197eef5
shivanimishra2208 <105953886+shivanimishra2208@users.noreply.github.com>
2022-07-29 18:23:38 -0600

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,7 @@
+const birthday = new Date;
+const date1 = birthday.getDate();
+const b =new Date
+const time = b.getHours() + ":"+ b.getMinutes()+ ":" +b.getSeconds();
+const date =b.toLocaleDateString()
+console.log(a)
+document.getElementById("time").innerHTML = time  +   "on" +    date   ;
